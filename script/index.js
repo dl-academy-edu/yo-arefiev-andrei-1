c2a41140a143ab4e41e04a36838588c88c804f89
Mcwain <129794168+Mcwain@users.noreply.github.com>
2023-06-11 23:39:48 +0400

    поправки

--- script/index.js
+++ script/index.js
@@ -1,10 +1,22 @@
 let modalWindowCloseBtn = document.querySelector ('.modalWindow__closeBtn_js');
 let modalWindowOpenBtn = document.querySelector ('.main__slider-btn_js');
 let modalWindow = document.querySelector ('.modalWindow_js');
+let modalWindowCloseBackground = document.querySelector ('.modalWindow__background_js');
 
 modalWindowOpenBtn.addEventListener('click', ()=> {
     modalWindow.classList.remove('hidden')
 })
+
 modalWindowCloseBtn.addEventListener('click', ()=> {
+    modalWindow.classList.add('hidden');
+})
+
+window.addEventListener('keydown', (e) => {
+    if (e.keyCode === 27) {
+        modalWindow.classList.add('hidden');
+    }
+})
+
+modalWindowCloseBackground.addEventListener('click', ()=> {
     modalWindow.classList.add('hidden');
 })
